96083b597b91f9bca1baa41509293b2c0a10f99e
gingerfawn <gingerfawnhenry@gmail.com>
2024-01-19 17:39:25 -0500

    draggable true 2

--- resources/views/livewire/side-nav.blade.php
+++ resources/views/livewire/side-nav.blade.php
@@ -10,7 +10,7 @@
                 @foreach($notes as $note)
                     @if($note->folder_id == $folder->id)
                     <div drag-note draggable="true" wire:key="note-{{ $note->id }}" drag-item="{{ $note->id }}">
-                        <a href="/?note={{ $note->id }}">{{$note->name}}</a> 
+                        <a draggable='true' href="/?note={{ $note->id }}">{{$note->name}}</a> 
                     </div>
                     @endif
                 @endforeach
