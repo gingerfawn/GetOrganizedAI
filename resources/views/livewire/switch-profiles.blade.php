8631e33cca5e8c7728f6756dc335fe51c710af4d
gingerfawn <gingerfawnhenry@gmail.com>
2024-09-06 13:11:26 -0400

    okay

--- resources/views/livewire/switch-profiles.blade.php
+++ resources/views/livewire/switch-profiles.blade.php
@@ -1,7 +1,9 @@
+
 <div class="select-profile">
     <select wire:model="selectedProfile" wire:change="updateSelectedProfile($event.target.value)" >
         @foreach($profiles as $profile)
-            <option value="{{ $profile->id }}" @if($profile->id == $current_profile->id) selected @endif>{{ $profile->name }}</option>
-        @endforeach
+            <option value="{{ $profile->id }}" @if($current_profile && $profile->id == $current_profile->id) selected @endif>{{ $profile->name }}</option>
+
+            @endforeach
     </select>
 </div>
